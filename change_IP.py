6869090cf92e6bda910ed84b89f30fc88120f424
yangkun <1269769296@qq.com>
2021-09-23 10:32:25 +0800

    update for CD

---
+++ change_IP.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+#coding=utf-8
+
+import glob, re, sys
+
+nargs = len(sys.argv)
+# print(sys.argv)
+if nargs != 2:
+    print("Usage: python3 change_IP.py IP\nFor example: python3 change_IP.py 192.168.1.126")
+    sys.exit()
+
+suffix = ["py", "sh", "launch", "bat"]
+file_path = [glob.glob('**/*.{}'.format(a), recursive=True) for a in suffix]
+file_path = [j for i in file_path for j in i]
+# print(file_path)
+
+rtext = sys.argv[1] #获取当前环境参数 即为$search_text 
+for f in file_path:
+    f1 = open(f,"r",encoding='UTF-8')
+    content = f1.read()
+    f1.close()
+
+    t = re.sub(r'192[\.]\d+[\.]\d+[\.]\d+', rtext, content)
+    with open(f,"w") as f2:
+        f2.write(t)
